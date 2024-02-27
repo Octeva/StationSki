109a5f1c08db601cdf6ca861624cfb902d0ecfeb
ophelieglnr <ophelie.goellner@isen.yncrea.fr>
2024-02-27 18:12:12 +0100

    dgjfyk

--- app/src/main/java/fr/isen/gauthier/projectgroup/Station/Bienvenu.kt
+++ app/src/main/java/fr/isen/gauthier/projectgroup/Station/Bienvenu.kt
@@ -3,11 +3,16 @@ package fr.isen.gauthier.projectgroup.Station
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.VelocityTracker
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.awaitTouchSlopOrCancellation
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -24,18 +29,33 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.IntOffset
 import fr.isen.gauthier.projectgroup.R
+import androidx.compose.ui.input.pointer.pointerInput
+
 
 
 class Bienvenu : ComponentActivity() {
@@ -50,7 +70,7 @@ class Bienvenu : ComponentActivity() {
 @Composable fun BasketView() {
     val context = LocalContext.current
 
-    Surface (
+    Surface(
         modifier = Modifier
             .fillMaxSize(),
     ) {
@@ -70,7 +90,7 @@ class Bienvenu : ComponentActivity() {
                 contentDescription = null,
                 modifier = Modifier
                     .padding(bottom = 16.dp)
-                    .size(200.dp) // Augmentation de la taille de l'image
+                    .size(350.dp) // Augmentation de la taille de l'image
             )
         }
     }
@@ -86,50 +106,63 @@ class Bienvenu : ComponentActivity() {
                 .shadow(elevation = 8.dp)
                 .background(color = colorResource(id = R.color.purple_500))
         ) {
-            OutlinedButton(onClick = {
-                val intent = Intent(context,PisteActivity::class.java)
-                context.startActivity(intent)
-            }){
+            OutlinedButton(
+                onClick = {
+                    val intent = Intent(context, PisteActivity::class.java)
+                    context.startActivity(intent)
+                },
+                modifier = Modifier.weight(1f)
+            ) {
                 Image(
                     painterResource(R.drawable._200px_alpine_skiing_pictogram_svg),
                     contentDescription = null
                 )
             }
-            OutlinedButton(onClick = {
-                val intent = Intent(context,RemonteActivity::class.java)
-                context.startActivity(intent)
-            }){
+            OutlinedButton(
+                onClick = {
+                    val intent = Intent(context, RemonteActivity::class.java)
+                    context.startActivity(intent)
+                },
+                modifier = Modifier.weight(1f)
+            ) {
                 Image(
                     painterResource(R.drawable._205016),
                     contentDescription = null
                 )
             }
-            OutlinedButton(onClick = {
-                Toast.makeText(context, "ici", Toast.LENGTH_LONG).show()
-            }){
+            OutlinedButton(
+                onClick = {
+                    Toast.makeText(context, "ici", Toast.LENGTH_LONG).show()
+                },
+                modifier = Modifier.weight(1f)
+            ) {
                 Image(
                     painterResource(R.drawable.picto_maison_png),
                     contentDescription = null
                 )
             }
-            OutlinedButton(onClick = {
-                Toast.makeText(context, "blabla", Toast.LENGTH_LONG).show()
-            }){
+            OutlinedButton(
+                onClick = {
+                    Toast.makeText(context, "blabla", Toast.LENGTH_LONG).show()
+                },
+                modifier = Modifier.weight(1f)
+            ) {
                 Image(
                     painterResource(R.drawable._58656_chat_icon_free_clipart_hd),
                     contentDescription = null
                 )
             }
-            OutlinedButton(onClick = {
-                Toast.makeText(context, "rafraiche", Toast.LENGTH_LONG).show()
-            }){
+            OutlinedButton(
+                onClick = {
+                    Toast.makeText(context, "rafraiche", Toast.LENGTH_LONG).show()
+                },
+                modifier = Modifier.weight(1f)
+            ) {
                 Image(
                     painterResource(R.drawable.refresh_icon),
                     contentDescription = null
                 )
             }
-
         }
     }
 }
-
