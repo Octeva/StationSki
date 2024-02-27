15e959ec77875ece17ba4e6009b5e085a0649569
ophelieglnr <ophelie.goellner@isen.yncrea.fr>
2024-02-27 14:16:18 +0100

    Update PisteActivity.kt

--- app/src/main/java/fr/isen/gauthier/projectgroup/Station/PisteActivity.kt
+++ app/src/main/java/fr/isen/gauthier/projectgroup/Station/PisteActivity.kt
@@ -6,8 +6,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -22,6 +26,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
@@ -76,16 +81,23 @@ fun test() {
 
     val colors = listOf(Color.Cyan, Color.Gray, Color.Red, Color.Green)
 
-    LazyColumn {
+    LazyColumn(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
         itemsIndexed(categories) { index, category ->
             val categoryColor = colors.getOrNull(index % colors.size) ?: Color.Transparent
             val backgroundColor = if (index == expandedCategoryIndex) categoryColor.copy(alpha = 0.5f) else categoryColor
 
-            Column(modifier = Modifier
-                .clickable {
-                    expandedCategoryIndex = index
-                }
-                .background(color = backgroundColor)
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .clickable {
+                        expandedCategoryIndex = index
+                    }
+                    .background(color = backgroundColor)
             ) {
                 Text(category.code)
                 if (expandedCategoryIndex == index) {
@@ -94,9 +106,12 @@ fun test() {
                     }
                 }
             }
+
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
+
 
 
 suspend fun getDataFromDatabase(): List<PisteCategory> {
