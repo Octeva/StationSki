76cef4a35175d795d34bf68e7fce45009c429e09
Octeva <94456561+Octeva@users.noreply.github.com>
2024-02-27 09:32:31 +0100

    aaaaaa

---
+++ app/src/main/java/fr/isen/gauthier/projectgroup/Station/DetailActivity.kt
@@ -0,0 +1,12 @@
+package fr.isen.gauthier.projectgroup.Station
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import fr.isen.gauthier.projectgroup.R
+
+class DetailActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
